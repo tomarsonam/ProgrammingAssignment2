dbc221a23a1be0fefd4e5f5d8dbc818b6afb0c77
tomarsonam <45241609+tomarsonam@users.noreply.github.com>
2020-05-17 13:48:53 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Assignment: Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
 
+## These two functions 
+## 1. create the cached matrix  
+## 2. cacluate the inverse or retrieve the cached inverse matrix if it has already been calculated. 
+
+## makeCacheMatrix sets and gets the value of the caching matrix
+## 1.  set the value of the matrix
+## 2.  get the value of the matrix
+## 3.  set the value of the inverse
+## 4.  get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
+  
+    j <- NULL
+    set <- function(y){
+      x <<- y
+      j <<- NULL
+    }
+    get <- function()x
+    setInverse <- function(inverse) j <<- inverse
+    getInverse <- function() j 
+    list(set = set, get = get, 
+         setInverse = setInverse, 
+         getInverse = getInverse)
 
 }
 
 
 ## Write a short comment describing this function
 
+##this function extraxt the inverse of the matrix created by above code
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
 }
+
+##How to run above code
+
+## m<- matrix(rnorm(4),2,2)
+## c <- makeCacheMatrix(m)
+## cacheSolve(c)
+
